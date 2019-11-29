4d6a3b3a105defd321ebd424f717a397c447b8ef
Preston <preston@prestonkemp.com>
2019-11-28 21:26:11 -0600

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Preston Kemp on 11/28/19.
+//  Copyright © 2019 Preston Kemp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
