cc7569a1b6232f790249a1cf2a1b5ee1a9f53b7b
Preston <preston@prestonkemp.com>
2019-11-28 21:43:14 -0600

    First commit!

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Hi there!", message: "Isn't it a beautiful day?", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction (title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 
 }
+
 
